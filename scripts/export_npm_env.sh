458d66636fd6a2c171c9b77d9d5ae0a5b9ab50db
Kudo Chien <ckchien@gmail.com>
2019-05-02 15:23:27 +0800

    Fix VERSION env not exported between runs

--- scripts/export_npm_env.sh
+++ scripts/export_npm_env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
+if [[ ${CIRCLECI} ]]; then
+    echo "export VERSION=${npm_package_version}" >> $BASH_ENV
+    echo "export V8_VERSION=${npm_package_config_V8}" >> $BASH_ENV
+else
+    export VERSION=${npm_package_version}
+    export V8_VERSION=${npm_package_config_V8}
+fi
 
-export VERSION=${npm_package_version}
-echo "export VERSION=${npm_package_version}"
-
-export V8_VERSION=${npm_package_config_V8}
-echo "export V8_VERSION=${npm_package_config_V8}"
