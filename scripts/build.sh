006e5f9b45931a57ec414502b6c3237327767753
Kudo Chien <ckchien@gmail.com>
2019-06-06 19:33:20 +0800

    Yet another trial to fix build break

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,8 +9,7 @@ GN_ARGS_BASE='
   use_debug_fission=false
   v8_expose_symbols=true
   use_custom_libcxx=false
-  use_sysroot=false
-  use_glib=false
+  use_custom_libcxx_for_host=false
 '
 
 if [[ "$BUILD_TYPE" = "Debug" ]]
