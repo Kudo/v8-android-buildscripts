42cc838174c86dd73ad8a8335d2c5da32fec8ce2
Kudo Chien <ckchien@gmail.com>
2019-11-14 11:12:22 +0800

    [WIP] troubleshooting CI error

--- scripts/export_npm_env.sh
+++ scripts/export_npm_env.sh
@@ -2,6 +2,9 @@
 if [[ ${CIRCLECI} ]]; then
   echo "export VERSION=${npm_package_version}" >> $BASH_ENV
   echo "export V8_VERSION=${npm_package_config_V8}" >> $BASH_ENV
+elif [[ ${GITHUB_ACTIONS} ]]; then
+  echo "::set-env name=VERSION::${npm_package_version}"
+  echo "::set-env name=V8_VERSION::${npm_package_config_V8}"
 else
   export VERSION=${npm_package_version}
   export V8_VERSION=${npm_package_config_V8}
