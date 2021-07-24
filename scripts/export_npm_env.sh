a42739bd5c620cf826d3ae213397b267c782b941
Kudo Chien <ckchien@gmail.com>
2021-07-24 20:46:20 +0800

    debug

--- scripts/export_npm_env.sh
+++ scripts/export_npm_env.sh
@@ -3,6 +3,7 @@ if [[ ${CIRCLECI} ]]; then
   echo "export VERSION=${npm_package_version}" >> $BASH_ENV
   echo "export V8_VERSION=${npm_package_config_V8}" >> $BASH_ENV
 elif [[ ${GITHUB_ACTIONS} ]]; then
+  ls -al $GITHUB_ENV
   echo "VERSION=${npm_package_version}" >> $GITHUB_ENV
   echo "V8_VERSION=${npm_package_config_V8}" >> $GITHUB_ENV
 else
